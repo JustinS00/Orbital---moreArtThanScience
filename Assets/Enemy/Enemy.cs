6ce117030246ef285897cd3576fd1c0b1f6d4452
lapselol <lapselol@gmail.com>
2022-06-19 18:40:09 +0800

    Add enemy drops && fix null exception on 0 arrows

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -19,6 +19,12 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     private bool isRanged = false;
 
+    [Header("Death Effects")]
+    [SerializeField]
+    private GameObject deathEffect;
+    [SerializeField]
+    private LootTable lootTable;
+
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -27,7 +33,7 @@ public class Enemy : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        LookAtPlayer(); 
+        LookAtPlayer();
         AttackPlayer();
     }
 
@@ -39,18 +45,17 @@ public class Enemy : MonoBehaviour {
 
     protected void LookAtPlayer() {
         Vector3 flipped = transform.localScale;
-		flipped.z *= -1f;
+        flipped.z *= -1f;
 
-		if (transform.position.x > player.transform.position.x && isFlipped) {
-			transform.localScale = flipped;
-			transform.Rotate(0f, 180f, 0f);
-			isFlipped = false;
-		}
-		else if (transform.position.x < player.transform.position.x && !isFlipped) {
-			transform.localScale = flipped;
-			transform.Rotate(0f, 180f, 0f);
-			isFlipped = true;
-		}
+        if (transform.position.x > player.transform.position.x && isFlipped) {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        } else if (transform.position.x < player.transform.position.x && !isFlipped) {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
     }
 
     public virtual void AttackPlayer() {
@@ -61,28 +66,35 @@ public class Enemy : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-		if (other.gameObject.tag == "Player") {
+        if (other.gameObject.tag == "Player") {
             /*
             knockback related
 			fromRight = other.transform.position.x < transform.position.x
 				? true
 				: false;	
             */
-			target = other.gameObject;
-			InvokeRepeating("Attack", 0.1f, 1.0f);
-		} 
-	}
+            target = other.gameObject;
+            InvokeRepeating("Attack", 0.1f, 1.0f);
+        }
+    }
 
-	private void OnCollisionExit2D(Collision2D other) {
-		if (other.gameObject.tag == "Player") {	
-			target = null;
-			CancelInvoke("Attack");
-		}
-	}
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            target = null;
+            CancelInvoke("Attack");
+        }
+    }
 
-	private void Attack() {
-		if (target != null) {
-			target.GetComponent<Health>().Damage(damage);
-		}
-	}
+    private void Attack() {
+        if (target != null) {
+            target.GetComponent<Health>().Damage(damage);
+        }
+    }
+
+    public void MakeLoot() {
+        if (lootTable) {
+            int randNumItems = Random.Range(0, data.maxItemsToDrop + 1);
+            lootTable.generateLoot(randNumItems, transform.position);
+        }
+    }
 }
