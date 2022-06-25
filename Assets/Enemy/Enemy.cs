716806a7dd74f817d31daef9dcac3f8f2bf82525
lapselol <lapselol@gmail.com>
2022-06-25 17:57:16 +0800

    Add chicken, cow, pig
    TODO: neutral mob loot table

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    public int damage = 5;
+    [SerializeField]
+    protected int damage = 5;
     [SerializeField]
     protected float speed = 1.5f;
 
