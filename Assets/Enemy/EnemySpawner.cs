8045db0d9096e18a02c090c12e85327df5dadb2e
lapselol <lapselol@gmail.com>
2022-07-19 16:55:04 +0800

    Add boss spawner behaviour

--- Assets/Enemy/EnemySpawner.cs
+++ Assets/Enemy/EnemySpawner.cs
@@ -7,6 +7,8 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField]
     private EntityCollection entityCollection;
     private GameObject[] entityPrefabs;
+    private int numberOfNormalMonsters = 2;
+    private float checkInterval = 10f;
 
     [SerializeField]
     private float spawnInterval = 15.0f;
@@ -14,7 +16,7 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField]
     private int maxNumEntities;
 
-    private int numberOfPossibleEntities; 
+    private int numberOfPossibleEntities;
     private Vector3 spawnLocation;
     private float randX;
     private string currentTag;
@@ -27,8 +29,9 @@ public class EnemySpawner : MonoBehaviour {
 
         entityPrefabs = entityCollection.prefabs;
 
-        numberOfPossibleEntities = entityPrefabs.Length;
+        numberOfPossibleEntities = numberOfNormalMonsters;
         currentTag = entityPrefabs[0].tag;
+
 
         StartCoroutine(spawnEntity(spawnInterval, entityPrefabs[Random.Range(0, numberOfPossibleEntities)]));
     }
@@ -47,7 +50,7 @@ public class EnemySpawner : MonoBehaviour {
         }
         maxNumEntities = OptionsMenu.instance.GetHostileMobCap();
         spawnLocation = new Vector3(player.transform.position.x + randX, player.transform.position.y + 10, 0);
-         //TODO: if player is in town or if time is day / do not spawn
+        //TODO: if player is in town or if time is day / do not spawn
         if (Physics2D.OverlapCircle(spawnLocation, 1.0f) == null && player.transform.position.y <= 128 && DayNightCycle.instance.isNight()) {
             GameObject newEntity = Instantiate(entity, spawnLocation, Quaternion.identity);
         }
@@ -55,5 +58,13 @@ public class EnemySpawner : MonoBehaviour {
         //limits number of enemies spawned to maxNumEntities
         yield return new WaitUntil(() => GameObject.FindGameObjectsWithTag(currentTag).Length < maxNumEntities);
         StartCoroutine(spawnEntity(spawnInterval, entityPrefabs[Random.Range(0, numberOfPossibleEntities)]));
+    }
+
+    public void EnableSlime() {
+        numberOfPossibleEntities = numberOfNormalMonsters + 1;
+    }
+
+    public void EnableCloth() {
+        numberOfPossibleEntities = numberOfNormalMonsters + 2;
     }
 }
--- Assets/Scripts/MobStats.cs
+++ Assets/Scripts/MobStats.cs
@@ -10,22 +10,33 @@ public class MobStats : MonoBehaviour {
     public static MobStats instance;
     public int[] kills;
     private int noOfMobs;
+    private GameObject entitySpawner;
 
     public void Awake() {
         if (instance == null) {
             noOfMobs = MobType.GetNames(typeof(MobType)).Length;
             kills = new int[noOfMobs];
             instance = this;
+            entitySpawner = GameObject.Find("EntitySpawner");
         } else {
             Destroy(gameObject);
         }
     }
 
-
     public void addKill(MobType type) {
         instance.kills[(int) type] += 1;
         if (type == MobType.pig && instance.kills[(int) MobType.pig] == 2) {
             Achievement.instance.UnlockAchievement(Achievement.AchievementType.technoblade);
+        }
+
+        if (type == MobType.slimeking) {
+            entitySpawner.GetComponent<EnemySpawner>().EnableSlime();
+            int dayNo = DayNightCycle.instance.GetDayNo();
+            entitySpawner.GetComponent<BossSpawner>().SetClothBossSpawnDay(dayNo + 10);
+        }
+
+        if (type == MobType.clothboss) {
+            entitySpawner.GetComponent<EnemySpawner>().EnableCloth();
         }
     }
 
